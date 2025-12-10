966fba114eafb6e3a0bd81e72870d7c977916201
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-12-10 11:08:34 +0000

    searching logic added

---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+use std::error::Error;
+use std::fs;
+use std::env;
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments. Usage: <program> <query> <filename>");
+        }
+
+        // Access query from command-line arguments
+        let query = &args[1];
+
+        // Filename from command-line arguments
+        let filename = &args[2];
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query: query.to_string(),
+            filename: filename.to_string(),
+            case_sensitive
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = match fs::read_to_string(&config.filename) {
+        Ok(content) => content,
+        Err(e) => {
+            eprintln!("Error reading file {}: {}", config.filename, e);
+            return Err(Box::new(e));
+        }
+    };
+
+    let result = if config.case_sensitive {
+        search_case_sensitive(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in result {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_sensitive(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+use cli_tool::Config;
+use cli_tool::run;
 
 fn main() {
     // Collect command-line arguments into a vector
@@ -22,38 +22,3 @@ fn main() {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = match fs::read_to_string(&config.filename) {
-        Ok(content) => content,
-        Err(e) => {
-            eprintln!("Error reading file {}: {}", config.filename, e);
-            return Err(Box::new(e));
-        }
-    };
-
-    println!("File contents:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments. Usage: <program> <query> <filename>");
-        }
-
-        // Access query from command-line arguments
-        let query = &args[1];
-
-        // Filename from command-line arguments
-        let filename = &args[2];
-        Ok(Config {
-            query: query.to_string(),
-            filename: filename.to_string(),
-        })
-    }
-}
